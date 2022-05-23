0005551c1861302dbd03725b50855347ef5d38e8
thutasan <thuta.sann@digitallaboratory.net>
2022-05-23 21:06:20 +0630

    first commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const Header = () => {
+  return (
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Thuta Sann</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">UI/UX Designer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img 
+        src="https://avatars.githubusercontent.com/u/102129896?v=4" alt="Thuta Sann" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {
+          [images.flutter, images.redux, images.sass].map((circle, index) => (
+            <div className='circle-cmp app__flex' key={`circle-${index}`}>
+              <img src={circle} alt="Profile Background" />
+            </div>
+          ))
+        }
+      </motion.div>
+    </div>
+  )
+}
+
+export default AppWrap(Header, 'home');
